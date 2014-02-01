93c2a53b19ea0288708493b5a9b1b0888b8ef3ca
Tony Forma <tony@agilelaw.com>
2014-02-01 09:14:44 -0600

    fix bugs with CURL adapater over SSL

--- src/httpAdapters/SagCURLHTTPAdapter.php
+++ src/httpAdapters/SagCURLHTTPAdapter.php
@@ -11,6 +11,7 @@ require_once('SagHTTPAdapter.php');
 
 class SagCURLHTTPAdapter extends SagHTTPAdapter {
   private $ch;
+  private $headers;
 
   public function __construct($host, $port) {
     if(!extension_loaded('curl')) {
@@ -18,22 +19,75 @@ class SagCURLHTTPAdapter extends SagHTTPAdapter {
     }
 
     parent::__construct($host, $port);
+  }
 
-    $this->ch = curl_init();
+  private function _setHeader($ch, $header)
+  {
+    $len=strlen($header);
+    $header=trim($header);
+
+    // skip 100 Continue header and blank lines
+    if ($header != 'HTTP/1.1 100 Continue' && $header != '')
+      $this->headers[] = trim($header);
+    return($len);
+  }
+
+  private function _read_cb($ch, $fd, $length)
+  {
+    return fread($fd,$length);
   }
 
   public function procPacket($method, $url, $data = null, $headers = array(), $specialHost = null, $specialPort = null) {
+    $this->ch = curl_init();
+    $this->headers = array();
+
     // the base cURL options
     $opts = array(
       CURLOPT_URL => "{$this->proto}://{$this->host}:{$this->port}{$url}",
       CURLOPT_PORT => $this->port,
       CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_HEADER => true,
+      CURLOPT_HEADER => false,
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_NOBODY => false,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => $method
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_HEADERFUNCTION => array($this, '_setHeader'),
     );
+
+    // send data through cURL's poorly named opt
+    if($data) {
+      if ($method=='PUT') {
+        // specify upload if it is a PUT request
+        $opts[CURLOPT_PUT] = true;
+        $opts[CURLOPT_UPLOAD] = true;
+
+        // chunking data because Couch disconnects as soon as it sends an HTTP error response
+        // which causes curl to throw an SSL_write error instead of processing the HTTP error
+        unset ($headers['Content-Length']);
+        unset ($headers['Expect']);
+        $headers['Transfer-Encoding'] = 'chunked';
+
+        // don't technically need this b/c curl will automatically add it
+        // but better to have it than to not realize it's being sent
+        $headers['Expect'] = '100-continue';
+
+        // _read_cb callback function is called to write data to the output stream
+        $opts[CURLOPT_READFUNCTION] = array($this, '_read_cb');
+
+        //create an in-memory file handle to read from
+        $fp = fopen('php://temp/maxmemory:256000', 'w');
+        if (!$fp) {
+          throw new SagException ("could not open temp memory data");
+        }
+        fwrite($fp, $data);
+        fseek($fp, 0);
+
+        $opts[CURLOPT_INFILE] =  $fp;
+        $opts[CURLOPT_INFILESIZE] = strlen($data);
+      } else {
+        $opts[CURLOPT_POSTFIELDS] = $data;
+      }
+    }
 
     // cURL wants the headers as an array of strings, not an assoc array
     if(is_array($headers) && sizeof($headers) > 0) {
@@ -42,11 +96,6 @@ class SagCURLHTTPAdapter extends SagHTTPAdapter {
       foreach($headers as $k => $v) {
         $opts[CURLOPT_HTTPHEADER][] = "$k: $v";
       }
-    }
-
-    // send data through cURL's poorly named opt
-    if($data) {
-      $opts[CURLOPT_POSTFIELDS] = $data;
     }
 
     // special considerations for HEAD requests
@@ -85,32 +134,35 @@ class SagCURLHTTPAdapter extends SagHTTPAdapter {
 
     $chResponse = curl_exec($this->ch);
 
+    // close file handle if opened
+    if ($fp)
+      fclose ($fp);
+
+    if ($curl_errno = curl_errno($this->ch)) {
+        $curl_error = curl_error($this->ch);
+    }
+    curl_close($this->ch);
+
     if($chResponse !== false) {
       // prepare the response object
       $response = new stdClass();
       $response->headers = new stdClass();
       $response->headers->_HTTP = new stdClass();
-      $response->body = '';
+      $response->body = $chResponse;
 
-      // split headers and body
-      list($headers, $response->body) = explode("\r\n\r\n", $chResponse);
-
-      // split up the headers
-      $headers = explode("\r\n", $headers);
-
-      for($i = 0; $i < sizeof($headers); $i++) {
+      for($i = 0; $i < sizeof($this->headers); $i++) {
         // first element will always be the HTTP status line
         if($i === 0) {
-          $response->headers->_HTTP->raw = $headers[$i];
+          $response->headers->_HTTP->raw = $this->headers[$i];
 
-          preg_match('(^HTTP/(?P<version>\d+\.\d+)\s+(?P<status>\d+))S', $headers[$i], $match);
+          preg_match('(^HTTP/(?P<version>\d+\.\d+)\s+(?P<status>\d+))S', $this->headers[$i], $match);
 
           $response->headers->_HTTP->version = $match['version'];
           $response->headers->_HTTP->status = $match['status'];
           $response->status = $match['status'];
         }
         else {
-          $line = explode(':', $headers[$i], 2);
+          $line = explode(':', $this->headers[$i], 2);
           $line[0] = strtolower($line[0]);
           $response->headers->$line[0] = ltrim($line[1]);
 
@@ -120,13 +172,14 @@ class SagCURLHTTPAdapter extends SagHTTPAdapter {
         }
       }
     }
-    else if(curl_errno($this->ch)) {
-      throw new SagException('cURL error #' . curl_errno($this->ch) . ': ' . curl_error($this->ch));
+    else if($curl_errno) {
+      throw new SagException('cURL error #' . $curl_errno . ': ' . $curl_error);
     }
     else {
       throw new SagException('cURL returned false without providing an error.');
     }
 
+    unset ($this->headers);
     return self::makeResult($response, $method);
   }
 }
